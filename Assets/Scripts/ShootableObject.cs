4ad49f6798b47558ec5487ca547202e64d132f7e
TimJeric1 <timjeric@hotmail.com>
2024-09-02 12:30:20 +0200

    Removed death animation,

--- Assets/Scripts/ShootableObject.cs
+++ Assets/Scripts/ShootableObject.cs
@@ -33,7 +33,7 @@ public class ShootableObject : MonoBehaviour
         {
             Debug.LogError("EnemyAI component not found on this GameObject.");
         }
-        
+
     }
 
     void Update()
@@ -42,7 +42,17 @@ public class ShootableObject : MonoBehaviour
         {
             if (destroyOnDeath)
             {
-                Destroy(gameObject);
+                // Trigger the "Dead" animation
+                animator.SetTrigger("Dead");
+
+                // Stop the enemy's movement
+                if (enemyAI != null)
+                {
+                    enemyAI.StopMovement();
+                }
+
+                // Start the coroutine to destroy the GameObject after 3 seconds
+                StartCoroutine(DestroyAfterDelay(0f));
             }
             else
             {
@@ -51,20 +61,7 @@ public class ShootableObject : MonoBehaviour
         }
     }
 
-    public void TakeShot()
-    {
-        // Trigger the "Dead" animation
-        animator.SetTrigger("Dead");
 
-        // Stop the enemy's movement
-        if (enemyAI != null)
-        {
-            enemyAI.StopMovement();
-        }
-
-        // Start the coroutine to destroy the GameObject after 3 seconds
-        StartCoroutine(DestroyAfterDelay(3f));
-    }
 
     private IEnumerator DestroyAfterDelay(float delay)
     {
